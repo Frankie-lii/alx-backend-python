9ea4e2c3d74bd0282e5d2f99a51532dda748b36d
Frank-003 <oyiengofrankline49@gmail.com>
2025-07-12 11:57:51 +0300

    task 0

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,7 +1,14 @@
-[LOG] Executing SQL query in function 'execute_query'...
-SQL: SELECT * FROM users WHERE active = 1;
-[LOG] Finished executing SQL query in 'execute_query'.
-[LOG] Executing SQL query in function 'execute_query'...
-SQL: SELECT * FROM users WHERE active = 1;
-[LOG] Finished executing SQL query in 'execute_query'.
+import functools
+
+def log_queries():
+        def decorator(func):
+                    @functools.wraps(func)
+                            def wrapper(*args, **kwargs):
+                                            print(f"[LOG] Executing SQL query in function '{func.__name__}'...")
+                                                        result = func(*args, **kwargs)
+                                                                    print(f"[LOG] Finished executing SQL query in '{func.__name__}'.")
+                                                                                return result
+                                                                                    return wrapper
+                                                                                    return decorator
+
 
