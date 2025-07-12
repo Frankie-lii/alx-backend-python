bf0cbde94c90bc665f62f8332b25185e53f55947
Frank-003 <oyiengofrankline49@gmail.com>
2025-07-12 11:54:25 +0300

    task 0

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,7 @@
+[LOG] Executing SQL query in function 'execute_query'...
+SQL: SELECT * FROM users WHERE active = 1;
+[LOG] Finished executing SQL query in 'execute_query'.
+[LOG] Executing SQL query in function 'execute_query'...
+SQL: SELECT * FROM users WHERE active = 1;
+[LOG] Finished executing SQL query in 'execute_query'.
+
