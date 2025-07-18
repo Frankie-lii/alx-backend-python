3dff0a461ede31ab522f9a4fca30486c9d98cfe6
Frankie-lii <oyiengofrankline49@gmail.com>
2025-07-18 09:15:02 +0000

    task 0

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,33 +1,3 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from .models import Conversation, Message
-from .serializers import ConversationSerializer, MessageSerializer
+from django.shortcuts import render
 
-
-class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
-    serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        # Automatically add the current user as a participant when creating a conversation
-        conversation = serializer.save()
-        conversation.participants.add(self.request.user)
-        conversation.save()
-
-
-class MessageViewSet(viewsets.ModelViewSet):
-    serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        # Messages for a specific conversation, optionally filtered
-        conversation_id = self.kwargs.get('conversation_pk')
-        return Message.objects.filter(conversation_id=conversation_id)
-
-    def perform_create(self, serializer):
-        # Send a message to a conversation
-        conversation_id = self.kwargs.get('conversation_pk')
-        serializer.save(sender=self.request.user, conversation_id=conversation_id)
-
+# Create your views here.
