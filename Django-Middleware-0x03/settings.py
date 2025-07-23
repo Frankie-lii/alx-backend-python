c2f1607bbe207e8e1a9e4f03a880758684c49c7b
Frankie-lii <oyiengofrankline49@gmail.com>
2025-07-23 14:10:46 +0000

    task 0

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -1,13 +1,14 @@
-INSTALLED_APPS = [
-    ...
-    'rest_framework',
-    'rest_framework_simplejwt',
-]
+MIDDLEWARE = [
+    # other default middlewares...
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ),
-}
+    # ✅ Your custom middleware
+    'chats.middleware.RequestLoggingMiddleware',
+]
 
