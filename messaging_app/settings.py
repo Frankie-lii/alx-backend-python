a47df7fa4bf52d1454057769c3b884a0eb42869a
Frankie-lii <oyiengofrankline49@gmail.com>
2025-07-23 08:10:17 +0000

    task 0

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -1,4 +1,20 @@
 INSTALLED_APPS = [
     ...
     'rest_framework',
-`]
+    'rest_framework_simplejwt',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
+
