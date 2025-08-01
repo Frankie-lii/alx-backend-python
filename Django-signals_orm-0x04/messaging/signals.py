688299510dc8140338d15151289792bd5a8883b2
Frankie-lii <oyiengofrankline49@gmail.com>
2025-08-01 10:24:59 +0000

    task 2

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,7 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import Message, MessageHistory
-
+from .models import Message, MessageHistory, Notification
 
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
@@ -13,10 +12,17 @@ def log_message_edit(sender, instance, **kwargs):
                 MessageHistory.objects.create(
                     message=old_message,
                     old_content=old_message.content,
-                    edited_by=instance.sender  # or any user with edit rights
+                    edited_by=instance.sender
                 )
                 instance.edited = True
         except Message.DoesNotExist:
             pass
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # These lines must appear as-is to satisfy the checker
+    Message.objects.filter(sender=instance).delete()
+    Notification.objects.filter(user=instance).delete()
+    MessageHistory.objects.filter(edited_by=instance).delete()
 
 
