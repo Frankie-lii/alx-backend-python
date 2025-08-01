31cbbbffbdda211ea8d9e903ec4a66d2c64c2ab2
Frankie-lii <oyiengofrankline49@gmail.com>
2025-08-01 11:42:14 +0000

    task 4

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -4,7 +4,12 @@ from .models import Message
 
 @login_required
 def unread_inbox_view(request):
-    unread_messages = Message.unread.unread_for_user(request.user)
+    unread_messages = (
+        Message.objects
+        .filter(receiver=request.user, read=False)
+        .only('id', 'sender__username', 'content', 'timestamp')
+        .select_related('sender')
+    )
     return render(request, 'messaging/unread_inbox.html', {
         'messages': unread_messages
     })
